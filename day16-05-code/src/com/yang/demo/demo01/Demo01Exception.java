e5efabc2b7e5a4787e3e8934677067a28e127dde
WinStarry <xuechao.yang@foxmail.com>
2020-09-08 21:13:07 +0800

    update data

--- day16-05-code/src/com/yang/demo/demo01/Demo01Exception.java
+++ day16-05-code/src/com/yang/demo/demo01/Demo01Exception.java
@@ -1,9 +1,10 @@
+package com.yang.demo.demo01;
+
+/*
 java.lang.Throwable类是Java中所有异常或错误的超类
     Exception:编译期异常，进行编译(写代码)时Java出现的问题
     RuntimeException:运行期异常，java程序在运行过程中出现的问题
     Error:错误，必须修改代码，程序才能运行
-
-
-/~~/
-
-
+ */
+public class Demo01Exception {
+}
